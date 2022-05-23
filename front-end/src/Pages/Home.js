58afa72d38fbce2565787b23e15609fa29c3083c
Sara D'Arce <saradarce@gmail.com>
2022-05-23 13:24:42 -0400

    A sense of normalcy landing page

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -3,20 +3,20 @@ import "./Home.css";
 function Home() {
   return (
     <main>
-      <div id="home-image"></div>
       <div id="main-content">
         <div id="featured-listings">
-        <div id="categories-menu">Browse by Category</div>
           Featured Listings (Carousel?)
         </div>
+        <div id="categories-menu">Browse by Category</div>
           {/* <FeaturedListing /> */}
         {/* <div id="home-listings-container">
           Browse All Listings
           <div id="listings-banner-image">
-            <div id="listings-banner-image-text"></div>
+          <div id="listings-banner-image-text"></div>
           </div>
-          <Listings /> */}
+        <Listings /> */}
       </div>
+        <div id="home-image"></div>
     </main>
   );
 }
